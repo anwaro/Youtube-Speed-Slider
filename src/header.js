2cbe37e83c6ab52b52ef1ffb1a3bbcc595b86962
Łukasz Micał <lukasz.mical@sarigato.com>
2017-10-23 23:48:39 +0200

    script - old version

---
+++ src/header.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Youtube Player Speed Slider
+// @namespace    youtube_player_speed_slider
+// @version      0.2.2
+// @description  Add Speed Slider to Youtube Player Settings
+// @author       Łukasz
+// @match        https://*.youtube.com/*
+// @grant        none
+// ==/UserScript==
